58ac2fc71e5980e597f478c7ee7224cdfd0d20f0
= <=>
2024-03-28 02:16:24 -0600

    Added bar chart

--- api-stay-smart/src/charts/getChartData.js
+++ api-stay-smart/src/charts/getChartData.js
@@ -16,7 +16,7 @@ exports.handler = async (event) => {
         }
         // All log statements are written to CloudWatch
         console.info('received:', event);
-        
+
         const params = {
             TableName: chartsTableName
         };
@@ -26,11 +26,38 @@ exports.handler = async (event) => {
         const cities = ["galle", "colombo", "negombo", "kandy", "nuwara eliya"];
         for (const city of cities) {
             const recordsForCity = data.Items.filter(item => item.city === city);
-            recordsForCity.sort((a, b) => b.predicted_proba - a.predicted_proba);
-            const firstFiveItems = array.slice(0, 5);
+            const uniqueHotels = recordsForCity.reduce((acc, obj) => {
+                acc.set(obj.hotelName, obj);
+                return acc;
+            }, new Map()).values();
+            console.log("Unique Hotels: ", uniqueHotels);
+            const uniqueHotelsArray = Array.from(uniqueHotels);
+            console.log("Unique Hotels Array: ", uniqueHotelsArray);
+            // uniqueHotelsArray.sort((a, b) => b.predicted_proba - a.predicted_proba);
+            const firstFiveItems = uniqueHotelsArray.slice(0, 5);
             results[city] = firstFiveItems;
         }
-        
+
+        const groupedData = data.Items.reduce((acc, obj) => {
+            const key = obj.hotelName;
+            if (!acc[key]) {
+                acc[key] = [];
+            }
+            acc[key].push(obj);
+            return acc;
+        }, {});
+        console.log("Grouped Data: ", groupedData);
+        let dataCount = {};
+        for (const [key, value] of Object.entries(groupedData)) {
+            dataCount[key] = value.length;
+        }
+        let barchartData = [];
+        for (const [key, value] of Object.entries(dataCount)) {
+            barchartData.push({ name: key, count: value });
+        }
+        const firstTenItems = barchartData.slice(0, 10);
+        results.barChart = firstTenItems;
+
         return common.getAPIResponseObj(event, results, "Chart data get successful", 200);
     } catch (error) {
         console.info(`error: `, error);
--- app/src/components/DashboardComponent.vue
+++ app/src/components/DashboardComponent.vue
@@ -11,39 +11,75 @@ export default {
     },
     data() {
         return {
-            chart_data: {
-                labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+            main_chart_data: {},
+            chart_one_data: {
+                labels: ['Loading...'],
                 datasets: [
                     {
-                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-                        data: [40, 20, 80, 10]
+                        backgroundColor: ['#41B883'],
+                        data: [0]
+                    }
+                ]
+            },
+            chart_two_data: {
+                labels: ['Loading...'],
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883'],
+                        data: [0]
+                    }
+                ]
+            },
+            chart_three_data: {
+                labels: ['Loading...'],
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883'],
+                        data: [0]
+                    }
+                ]
+            },
+            chart_four_data: {
+                labels: ['Loading...'],
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883'],
+                        data: [0]
+                    }
+                ]
+            },
+            chart_five_data: {
+                labels: ['Loading...'],
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883'],
+                        data: [0]
                     }
                 ]
             },
             chart_options: {
                 responsive: true,
-                maintainAspectRatio: false
+                maintainAspectRatio: false,
+                scales: {
+                    y: [
+                        {
+                            ticks: {
+                                beginAtZero: false,
+                                padding: 0,
+                            }
+                        }
+                    ]
+                }
             },
             barchart_data: {
                 labels: [
-                    'January',
-                    'February',
-                    'March',
-                    'April',
-                    'May',
-                    'June',
-                    'July',
-                    'August',
-                    'September',
-                    'October',
-                    'November',
-                    'December'
+                    'Loading...'
                 ],
                 datasets: [
                     {
-                        label: 'Data One',
+                        label: 'Top Hotels',
                         backgroundColor: '#f87979',
-                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+                        data: [0]
                     }
                 ]
             }
@@ -52,125 +88,81 @@ export default {
     created() {
     },
     methods: {
-        async search() {
-            if (this.selectedItem != null) {
-                try {
-                    this.search_results = [];
-                    this.user_preferences_results = [];
-                    this.page_title = this.msg_your_search_results;
-                    this.page_sub_title = this.msg_loading;
-                    
-                    const calls = [];
-                    calls.push(this.getTopHotels(this.search_text, this.selectedItem));
-                    calls.push(this.getUserPreferences(this.selectedItem));
-                    Promise.all(calls).then(() => {
-                        console.log('search completed');
-                    })
-                } catch (error) {
-                    console.log(error)
-                }
-            }
-        },
-        async getTopHotels (description = '', location = '') {
+        async getChartData() {
             try {
-                this.page_title = this.msg_hot_picks_search;
-                this.page_sub_title = this.msg_loading;
-                
-                const userEmail = localStorage.getItem('x-user');
-                const response = await axios.get('https://jr0m39z7w3.execute-api.ca-central-1.amazonaws.com/Prod/hotels/top?description=' + description + '&location=' + location + '&email=' + userEmail);
-                response.data.data.forEach(item => {
-                    this.search_results.push({
-                        hotelUrl: item["HotelUrl"],
-                        imgUrl: item["ImgUrl"],
-                        hotelName: item["Hotel Name"],
-                        city: item["City"],
-                        reviews: item["reviews"],
-                        date: item["Date"]
-                    })
-                });
-                if (response.data.data.length === 0) {
-                    this.page_sub_title = this.msg_no_items_found;
-                }
+                const response = await axios.get('https://jr0m39z7w3.execute-api.ca-central-1.amazonaws.com/Prod/charts/top-hotels');
+                this.main_chart_data = response.data.data;
+                this.chart_one_data = this.getPieChartData(this.main_chart_data.galle);
+                this.chart_two_data = this.getPieChartData(this.main_chart_data.colombo);
+                this.chart_three_data = this.getPieChartData(this.main_chart_data.negombo);
+                this.chart_four_data = this.getPieChartData(this.main_chart_data.kandy);
+                this.chart_five_data = this.getPieChartData(this.main_chart_data['nuwara eliya']);
+                this.barchart_data = this.getBarChartData(this.main_chart_data.barChart);
             } catch (error) {
-                this.page_sub_title = this.msg_no_items_found;
+                console.log(error);
             }
         },
-        async getUserPreferences (location) {
-            try {
-                this.page_title = this.msg_hot_picks_search;
-                this.page_sub_title = this.msg_loading;
-                
-                const userEmail = localStorage.getItem('x-user');
-                const response = await axios.get('https://jr0m39z7w3.execute-api.ca-central-1.amazonaws.com/Prod/hotels/user-preferences?email=' + userEmail + '&location=' + location);
-                response.data.data.forEach(item => {
-                    this.user_preferences_results.push({
-                        hotelUrl: item["HotelUrl"],
-                        imgUrl: item["ImgUrl"],
-                        hotelName: item["Hotel Name"],
-                        city: item["City"],
-                        reviews: item["reviews"],
-                        date: item["Date"]
-                    })
-                });
-                if (response.data.data.length === 0) {
-                    this.page_sub_title = this.msg_no_items_found;
-                }
-            } catch (error) {
-                this.page_sub_title = this.msg_no_items_found;
+        getPieChartData(data) {
+            let dynamicRecord = {
+                labels: data.map(item => item.hotelName),
+                datasets: [
+                    {
+                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16', '#41B883'],
+                        data: data.map(item => parseFloat(item.predicted_proba))
+                    }
+                ]
             }
+            return dynamicRecord;
         },
-        searchOnEnter(e) {
-            if (e.key == 'Enter') {
-                this.search()
+        getBarChartData(data) {
+            let dynamicRecord = {
+                labels: data.map(item => item.name),
+                datasets: [
+                    {
+                        label: 'Top Hotels',
+                        backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16', '#41B883'],
+                        data: data.map(item => parseFloat(item.count))
+                    }
+                ]
             }
-        },
-        clear() {
-            this.page_title = this.msg_hot_picks_search;
-            this.search_results = []
-            this.search_text = ''
-            this.selectedItem = null
-            localStorage.removeItem('search')
-
-            const calls = [];
-            calls.push(this.getTopHotels());
-            calls.push(this.getUserPreferences(this.selectedItem));
-            Promise.all(calls).then(() => {
-                console.log('search completed');
-            })
-        },
+            return dynamicRecord;
+        }
     },
     mounted() {
+        this.getChartData();
     },
 }
 </script>
 
 <template>
-    <div>
+    <div class="chart">
         <Bar id="barchart" :data="barchart_data" :options="chart_options" />
     </div>
-    <div>
-        <Pie id="chart1" :data="chart_data" :options="chart_options" />
+    <div class="chart">
+        <Pie id="chart1" :data="chart_one_data" :options="chart_options" />
     </div>
-    <div>
-        <Pie id="chart2" :data="chart_data" :options="chart_options" />
+    <div class="chart">
+        <Pie id="chart2" :data="chart_two_data" :options="chart_options" />
     </div>
-    <div>
-        <Pie id="chart3" :data="chart_data" :options="chart_options" />
+    <div class="chart">
+        <Pie id="chart3" :data="chart_three_data" :options="chart_options" />
     </div>
-    <div>
-        <Pie id="chart4" :data="chart_data" :options="chart_options" />
+    <div class="chart">
+        <Pie id="chart4" :data="chart_four_data" :options="chart_options" />
     </div>
-    <div>
-        <Pie id="chart5" :data="chart_data" :options="chart_options" />
+    <div class="chart">
+        <Pie id="chart5" :data="chart_five_data" :options="chart_options" />
     </div>
-    
+
 </template>
 
 <style scoped>
-
-.search-results{
+.search-results {
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
+}
+.chart{
+    margin: 20px 0px 40px 0px;
 }
 </style>
